a890ccad575056856ddbe86140c04adbe90a6be0
akshaykhoje <code.akshaykhoje@gmail.com>
2023-12-24 21:57:21 +0530

    return todo by ID works

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -39,10 +40,31 @@ func addTodo(context *gin.Context) {
 	context.IndentedJSON(http.StatusCreated, newTodo)
 }
 
+// first we need to extract the 'id' from the POST request URL which is there in `context`
+func getTodo(context *gin.Context) {
+	id := context.Param("id")
+	todo, err := getTodoById(id)
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo not found"})
+	}
+	context.IndentedJSON(http.StatusOK, todo)
+}
+
+func getTodoById(id string) (*todo, error) {
+	for i, t := range todos {
+		if t.ID == id {
+			return &todos[i], nil
+		}
+	}
+	return nil, errors.New("Todos not found!")
+}
+
 func main() {
 	router := gin.Default()
 	// router.GET("/", displayRoot)
 	router.GET("/todos", getTodos)
+	router.GET("/todos/:id", getTodo) // the ':' tells that field is dynamic and is called as 'id' here
 	router.POST("/todos", addTodo)
 	router.Run("localhost:9090")
 
